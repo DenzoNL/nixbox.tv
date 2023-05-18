dc6f0774db6bc8f3aa730d0efef073f75ff39a35
= <=>
2023-05-18 15:55:59 +0200

    add vscode server support, add hardware-configuration (temporarily)

--- flake.nix
+++ flake.nix
@@ -6,14 +6,21 @@
     nixpkgs = {
       url = "github:NixOS/nixpkgs/nixos-unstable";
     };
+    vscode-server = { 
+      url = "github:nix-community/nixos-vscode-server";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, vscode-server }: {
     nixosConfigurations = {
       nixbox = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
+          vscode-server.nixosModule
+          ({ config, pkgs, ... }: {
+            services.vscode-server.enable = true;
+          })
         ];
       };
     };
