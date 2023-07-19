f0e9f9296d37a503cd0ebf70892ea4a8ea8f3d2a
Dennis Bogers <d.bogers@korsit.com>
2023-07-19 22:26:43 +0200

    remove podman

--- hosts/nixbox/configuration.nix
+++ hosts/nixbox/configuration.nix
@@ -116,16 +116,6 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  virtualisation = {
-    podman = {
-      enable = true;
-      dockerCompat = true;
-      defaultNetwork.settings = {
-        dns_enabled = true;
-      };
-    };
-  };
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
