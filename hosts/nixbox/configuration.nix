e6d2ffd37b8add3ccab55d7ffb19bc83d25b518b
Dennis Bogers <d.bogers@korsit.com>
2024-04-26 22:48:39 +0000

    feat: replace zed email notifications with ntfy

--- hosts/nixbox/configuration.nix
+++ hosts/nixbox/configuration.nix
@@ -28,7 +28,6 @@
     defaultSopsFile = ./secrets.yaml;
     age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
     secrets = {
-      "smtp/password" = {};
       "borg/ssh_private_key" = {};
       "borg/passphrase" = {};
     };
@@ -129,38 +128,16 @@
     };
   };
 
-  programs.msmtp = {
-    enable = true;
-    setSendmail = true;
-    defaults = {
-      aliases = "/etc/aliases";
-      port = 465;
-      tls_trust_file = "/etc/ssl/certs/ca-certificates.crt";
-      tls = "on";
-      auth = "login";
-      tls_starttls = "off";
-    };
-    accounts = {
-      default = {
-        host = "smtp.gmail.com";
-        passwordeval = "cat ${config.sops.secrets."smtp/password".path}";
-        user = "denzonl@gmail.com";
-        from = "denzonl@gmail.com";
-      };
-    };
-  };
-
   services.zfs.zed.settings = {
     ZED_DEBUG_LOG = "/tmp/zed.debug.log";
-    ZED_EMAIL_ADDR = [ "root" ];
-    ZED_EMAIL_PROG = "${pkgs.msmtp}/bin/msmtp";
-    ZED_EMAIL_OPTS = "@ADDRESS@";
-
     ZED_NOTIFY_INTERVAL_SECS = 3600;
     ZED_NOTIFY_VERBOSE = true;
 
     ZED_USE_ENCLOSURE_LEDS = true;
     ZED_SCRUB_AFTER_RESILVER = true;
+
+    ZED_NTFY_TOPIC = "nixbox";
+    ZED_NTFY_URL = "https://ntfy.nixbox.tv";
   };
 
   # Back up to borgbase
