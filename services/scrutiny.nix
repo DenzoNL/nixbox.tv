2ff777e6fcad345af567dfba00f05ad4a9f79cd5
Dennis Bogers <d.bogers@korsit.com>
2024-06-02 12:04:37 +0000

    enable scrutiny smart notifications

--- services/scrutiny.nix
+++ services/scrutiny.nix
@@ -3,8 +3,11 @@
 {
   services.scrutiny = {
     enable = true;
-    settings.web.listen.port = 8181;
     collector.enable = true;
+    settings.web.listen.port = 8181;
+    settings.notify.urls = [
+      "ntfy://ntfy.nixbox.tv/nixbox"
+    ];
   };
 
   services.nginx.virtualHosts."scrutiny.nixbox.tv" = {
