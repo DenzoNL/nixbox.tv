cc94970c36b211336bf37a122adf0e54d6f96925
Dennis Bogers <d.bogers@korsit.com>
2024-07-27 19:01:12 +0000

    feat: cs2 server

--- services/cs2.nix
+++ services/cs2.nix
@@ -1,15 +1,81 @@
-{ ... }:
+{ pkgs, ... }:
 
+let
+  cfg = {
+    stateDir = "/var/lib/cs2";
+    hostname = "De Frietzaak";
+    tickrate = 128;
+    user = "cs2";
+    group = "cs2";
+    port = 27015;
+  };
+
+  createSteamLink = ''
+    ln -sfv ${cfg.stateDir}/.steam/steam/linux64 ${cfg.stateDir}/.steam/sdk64
+  '';
+
+  patchElf = ''
+    ${pkgs.patchelf}/bin/patchelf \
+      --set-interpreter "$(cat ${pkgs.stdenv.cc}/nix-support/dynamic-linker)" \
+      --set-rpath "${cfg.stateDir}/game/bin/linuxsteamrt64" \
+      ${cfg.stateDir}/game/bin/linuxsteamrt64/cs2
+  '';
+
+  steamCmdUpdate = ''
+    ${pkgs.steamcmd}/bin/steamcmd \
+      +force_install_dir ${cfg.stateDir} \
+      +login anonymous \
+      +app_update 730 \
+      +quit
+  '';
+in
 {
-  virtualisation.oci-containers.backend = "docker";
-  virtualisation.docker.enable = true;
-  virtualisation.oci-containers.containers."cs2" = {
-    image = "cs2-modded-server";
-    autoStart = false;
-    volumes = [
-      "cs2-volume:/home/steam/"
-      "/home/denzo/custom_files:/home/custom_files/"
-      "/home/denzo/game:/home/game/"
-    ];
+  users.users.cs2 = {
+    isSystemUser = true;
+    home = cfg.stateDir;
+    createHome = true;
+    group = cfg.group;
+  };
+
+  users.groups.cs2 = {};
+
+  networking.firewall.allowedTCPPorts = [ cfg.port ];
+  networking.firewall.allowedUDPPorts = [ cfg.port ];
+
+  systemd.services.cs2 = {
+    description = "Counter-Strike 2 Dedicated Server";
+    wantedBy = [ "multi-user.target" ];
+    environment = {
+      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+        "${cfg.stateDir}/game/bin/linuxsteamrt64"
+        "${pkgs.util-linux.lib}"
+        "${pkgs.stdenv.cc.cc.lib}"
+      ];    
+    };
+    preStart = ''
+      ${steamCmdUpdate}
+      ${createSteamLink}
+      ${patchElf}
+    '';
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = ''
+        ${cfg.stateDir}/game/bin/linuxsteamrt64/cs2 \
+          -dedicated \
+          -port ${toString cfg.port} \
+          -tickrate ${toString cfg.tickrate} \
+          +map de_dust2 \
+          +hostname ${cfg.hostname} \
+          +sv_lan 0 \
+          +sv_logfile 1 \
+          +game_alias deathmatch
+      '';
+      TimeoutStartSec = 0;
+      RestartSec = "120s";
+      Restart = "always";
+      User = cfg.user;
+      Group = cfg.group;
+      WorkingDirectory = "${cfg.stateDir}";
+    };
   };
 }
