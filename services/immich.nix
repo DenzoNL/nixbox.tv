f954e672abf60c2353ee62557f446a69677d9e68
Dennis Bogers <d.bogers@korsit.com>
2024-10-15 21:16:43 +0000

    fix: additional proxy settings to make immich file upload work correctly

--- services/immich.nix
+++ services/immich.nix
@@ -10,5 +10,14 @@
       proxyPass = "http://localhost:3001/";
       proxyWebsockets = true;
     };
+    extraConfig = ''
+      # Allow large file uploads
+      client_max_body_size 50000M;
+
+      # Configure timeout
+      proxy_read_timeout 600s;
+      proxy_send_timeout 600s;
+      send_timeout       600s;
+    '';
   };
 }
