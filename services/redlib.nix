5afd814d7ea27c2192ca11df41a386de19c53445
Dennis Bogers <d.bogers@korsit.com>
2024-10-23 21:16:18 +0000

    refactor: update redlib to use new NixOS service

--- services/redlib.nix
+++ services/redlib.nix
@@ -1,36 +1,14 @@
-{ pkgs, ... }:
+{ config, ... }:
 
 {
-  users.users.redlib = {
-    group = "redlib";
-    shell = pkgs.bashInteractive;
-    home = "/var/lib/redlib";
-    description = "redlib daemon user";
-    isSystemUser = true;
-  };
-
-  users.groups.redlib = {};
-
-  systemd.services.redlib = {
+  services.redlib = {
     enable = true;
-    wantedBy = [ "multi-user.target" ];
-    after = [ "network.target" ];
-    before = [ "nginx.service" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.redlib}/bin/redlib -a 0.0.0.0 -p 8069";
-      User = "redlib";
-      Group = "redlib";
-      Type = "simple";
-      Restart = "on-failure";
-      WorkingDirectory = "/var/lib/redlib";
-    };
+    port = 8069;
   };
-
-  systemd.tmpfiles.rules = [ "d '/var/lib/redlib' 0755 redlib redlib -" ];
 
   services.nginx.virtualHosts."reddit.nixbox.tv" = {
     locations."/" = {
-      proxyPass = "http://localhost:8069/";
+      proxyPass = "http://localhost:${toString config.services.redlib.port}/";
       proxyWebsockets = true;
     };
   };
