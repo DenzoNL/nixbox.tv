3653b51b5a8cfaac69fb71a075de8f3b841e82b0
Dennis Bogers <d.bogers@korsit.com>
2024-10-24 18:56:08 +0000

    chore: update flake.lock

--- hosts/nixbox/configuration.nix
+++ hosts/nixbox/configuration.nix
@@ -45,7 +45,7 @@
 
   # Enable Nix Flakes
   nix = {
-    package = pkgs.nixFlakes;
+    package = pkgs.nixVersions.stable;
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
