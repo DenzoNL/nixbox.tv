6763226e7f6adf4df71301999b7e811df5434578
Dennis Bogers <d.bogers@korsit.com>
2025-01-25 00:19:59 +0000

    chore: upgrade streammaster

--- services/streammaster.nix
+++ services/streammaster.nix
@@ -2,7 +2,7 @@
 
 {
   virtualisation.oci-containers.containers.streammaster = {
-    image = "senexcrenshaw/streammaster:net9-1.0.0.4";
+    image = "senexcrenshaw/streammaster:main-1.0.1.7";
     ports = ["127.0.0.1:7095:7095"];
     volumes = [
       "/var/lib/streammaster:/config"
