f9c128dccb9a5ea3fb9e48ccdf966de6f28d7dfd
Dennis Bogers <d.bogers@korsit.com>
2025-03-06 22:14:00 +0000

    feat: add zigbee2mqtt

--- services/home-assistant.nix
+++ services/home-assistant.nix
@@ -66,12 +66,43 @@
     ];
   };
 
+  sops.secrets."zigbee2mqtt/secret.yaml" = {
+    owner = config.users.users.zigbee2mqtt.name;
+  };
+
+  services.zigbee2mqtt = {
+    enable = true;
+    settings = {
+      homeassistant = config.services.home-assistant.enable;
+      permit_join = true;
+      mqtt = {
+        user = "hass";
+        password = "!${config.sops.secrets."zigbee2mqtt/secret.yaml".path} password";
+      };
+      serial = {
+        port = "/dev/ttyUSB0";
+        adapter = "zstack";
+      };
+      frontend = {
+        enabled = true;
+        port = 8083;
+      };
+    };
+  };
+
   services.nginx.virtualHosts."home.nixbox.tv" = {
     extraConfig = ''
       proxy_buffering off;
     '';
     locations."/" = {
       proxyPass = "http://localhost:8123";
+      proxyWebsockets = true;
+    };
+  };
+
+  services.nginx.virtualHosts."zigbee2mqtt.nixbox.tv" = {
+    locations."/" = {
+      proxyPass = "http://localhost:8083";
       proxyWebsockets = true;
     };
   };
