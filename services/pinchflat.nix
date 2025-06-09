2c474434f989f6b1057128d71fe0f0599df9b606
Dennis Bogers <d.bogers@korsit.com>
2025-06-09 17:50:37 +0000

    feat: pinchflat

--- services/pinchflat.nix
+++ services/pinchflat.nix
@@ -1,16 +1,30 @@
-{ config, domain, ... }:
+{ config, domain, lib, ... }:
 
 {
   services.pinchflat = {
-    enable = false;
+    enable = true;
     selfhosted = true;
     mediaDir = "/mnt/storage/youtube";
   };
 
-  # services.nginx.virtualHosts."pinchflat.${domain}" = {
-  #   locations."/" = {
-  #     proxyPass = "http://localhost:${toString config.services.pinchflat.port}/";
-  #     proxyWebsockets = true;
-  #   };
-  # };
+  users.users.pinchflat = {
+    isSystemUser = true;
+    group = "pinchflat";
+  };
+
+  users.groups.pinchflat = {};
+
+  users.groups.mediausers = {
+    members = [ "pinchflat" ];
+  };
+
+  systemd.services.pinchflat.serviceConfig.User = "pinchflat";
+  systemd.services.pinchflat.serviceConfig.DynamicUser = lib.mkForce false;
+
+  services.nginx.virtualHosts."pinchflat.${domain}" = {
+    locations."/" = {
+      proxyPass = "http://localhost:${toString config.services.pinchflat.port}/";
+      proxyWebsockets = true;
+    };
+  };
 }
